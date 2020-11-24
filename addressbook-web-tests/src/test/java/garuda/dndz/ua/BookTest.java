22ba2f60382e65f222fa2064f73d164a0f7a5fa2
garudapurana <73479834+garudapurana@users.noreply.github.com>
2020-11-24 14:57:20 +0200

    refactoring
    выделенно методы и класс конструктор

--- addressbook-web-tests/src/test/java/garuda/dndz/ua/BookTest.java
+++ addressbook-web-tests/src/test/java/garuda/dndz/ua/BookTest.java
@@ -7,38 +7,66 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.Dimension;
-import org.openqa.selenium.JavascriptExecutor;
+
+import java.util.concurrent.TimeUnit;
+
 
 public class BookTest {
-  private WebDriver driver;
-  JavascriptExecutor js;
+  WebDriver wd;
+
   @Before
   public void setUp() {
-    driver = new ChromeDriver();
-    js = (JavascriptExecutor) driver;
+    wd = new ChromeDriver();
+    wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    wd.manage().window().setSize(new Dimension(1060, 1030));
+    wd.get("http://localhost/");
+    login("admin", "secret");
   }
+
+  private void login(String ussername, String password) {
+    wd.findElement(By.name("user")).sendKeys(ussername);
+    wd.findElement(By.name("pass")).sendKeys(password);
+    wd.findElement(By.cssSelector("input:nth-child(7)")).click();
+  }
+
+  @Test
+  public void addgroupTest() {
+    gotoGroupPage();
+    initGroupCreation();
+    fillGroupForm(new GroupData("asdasdasdasdasd", "123asd", "asd123"));
+    submitGroupCreation();
+    gotoGroupPage();
+    logout();
+  }
+
+  private void logout() {
+    wd.findElement(By.linkText("Logout")).click();
+  }
+
+  private void submitGroupCreation() {
+    wd.findElement(By.name("submit")).click();
+  }
+
+  private void fillGroupForm(GroupData groupData) {
+    wd.findElement(By.name("group_name")).click();
+    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
+    wd.findElement(By.name("group_header")).click();
+    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+    wd.findElement(By.name("group_footer")).click();
+    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+  }
+
+  private void initGroupCreation() {
+    wd.findElement(By.name("new")).click();
+  }
+
+  private void gotoGroupPage() {
+    wd.findElement(By.linkText("groups")).click();
+  }
+
   @After
   public void tearDown() {
-    driver.quit();
-  }
-  @Test
-  public void qwe() {
-    driver.get("http://localhost/");
-    driver.manage().window().setSize(new Dimension(825, 1030));
-    driver.findElement(By.name("user")).sendKeys("admin");
-    driver.findElement(By.name("pass")).sendKeys("secret");
-    driver.findElement(By.cssSelector("input:nth-child(7)")).click();
-    driver.findElement(By.linkText("groups")).click();
-    driver.findElement(By.name("new")).click();
-    driver.findElement(By.name("group_name")).click();
-    driver.findElement(By.name("group_name")).sendKeys("asdasdasdasdasd");
-    driver.findElement(By.name("group_header")).click();
-    driver.findElement(By.name("group_header")).sendKeys("123asd");
-    driver.findElement(By.name("group_footer")).click();
-    driver.findElement(By.name("group_footer")).sendKeys("asd123");
-    driver.findElement(By.name("submit")).click();
-    driver.findElement(By.linkText("groups")).click();
-    driver.findElement(By.linkText("Logout")).click();
+    wd.quit();
   }
 }
 
