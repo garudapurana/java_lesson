f90d0e922b9059e6370f077c5be4bbb56e45b9d5
garudapurana <73479834+garudapurana@users.noreply.github.com>
2020-11-27 14:59:07 +0200

    запуск в разных браузерах
    реализован выбор запуска браузеров

--- addressbook-web-tests/src/test/java/garuda/dndz/ua/appmanager/AplicationManager.java
+++ addressbook-web-tests/src/test/java/garuda/dndz/ua/appmanager/AplicationManager.java
@@ -3,6 +3,9 @@ package garuda.dndz.ua.appmanager;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 
 public class AplicationManager {
@@ -12,8 +15,22 @@ public class AplicationManager {
   private  NavigationHelper navigationHelper;
   private  GroupHelper groupHelper;
   private  SessionManager sessionManager;
+  private String browser;
+
+  public AplicationManager(String browser) {
+    this.browser = browser;
+  }
+
   public void init() {
-    wd = new ChromeDriver();
+
+    if(browser==BrowserType.CHROME){
+      wd = new ChromeDriver();
+    }else if(browser==BrowserType.FIREFOX){
+      wd = new FirefoxDriver();
+    }else if(browser==BrowserType.IE){
+      wd = new InternetExplorerDriver();
+    }
+
     wd.manage().window().setSize(new Dimension(1060, 1030));
     wd.get("http://localhost/");
     groupHelper = new GroupHelper(wd);
--- addressbook-web-tests/src/test/java/garuda/dndz/ua/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/garuda/dndz/ua/tests/TestBase.java
@@ -3,10 +3,11 @@ package garuda.dndz.ua.tests;
 import garuda.dndz.ua.appmanager.AplicationManager;
 import org.junit.After;
 import org.junit.Before;
+import org.openqa.selenium.remote.BrowserType;
 
 public class TestBase {
 
-  protected final AplicationManager app = new AplicationManager();
+  protected final AplicationManager app = new AplicationManager(BrowserType.CHROME);
 
   @Before
   public void setUp() {
