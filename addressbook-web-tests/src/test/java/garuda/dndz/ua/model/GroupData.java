0fcf994ad561d48e750fc8d240ec12d246739b9d
garudapurana <73479834+garudapurana@users.noreply.github.com>
2020-12-03 21:17:30 +0200

    44 Множества Неупорядоченные коллекции
    44 Множества Неупорядоченные коллекции

--- addressbook-web-tests/src/test/java/garuda/dndz/ua/model/GroupData.java
+++ addressbook-web-tests/src/test/java/garuda/dndz/ua/model/GroupData.java
@@ -3,11 +3,41 @@ package garuda.dndz.ua.model;
 import java.util.Objects;
 
 public class GroupData {
+  private final String id;
   private final String name;
   private final String header;
   private final String footer;
 
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    return Objects.equals(id, groupData.id) && Objects.equals(name, groupData.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name);
+  }
+
+  @Override
+  public String toString() {
+    return "GroupData{" +
+            "id='" + id + '\'' +
+            ", name='" + name + '\'' +
+            '}';
+  }
+
+  public GroupData(String id, String name, String header, String footer) {
+    this.id = id;
+    this.name = name;
+    this.header = header;
+    this.footer = footer;
+  }
   public GroupData(String name, String header, String footer) {
+    this.id = null;
     this.name = name;
     this.header = header;
     this.footer = footer;
@@ -17,26 +47,6 @@ public class GroupData {
     return name;
   }
 
-  @Override
-  public String toString() {
-    return "GroupData{" +
-            "name='" + name + '\'' +
-            '}';
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    GroupData groupData = (GroupData) o;
-    return Objects.equals(name, groupData.name);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(name);
-  }
-
   public String getHeader() {
     return header;
   }
@@ -44,4 +54,5 @@ public class GroupData {
   public String getFooter() {
     return footer;
   }
+  public String getId() { return id; }
 }
--- addressbook-web-tests/src/test/java/garuda/dndz/ua/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/garuda/dndz/ua/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import garuda.dndz.ua.model.GroupData;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase{
@@ -17,11 +18,17 @@ public class GroupModificationTests extends TestBase{
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(before.size()-1);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+    GroupData group = new GroupData(before.get(before.size()-1).getId(),"test1", "test2", "test3");
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(before.size(),after.size());
+
+    before.remove(before.size()-1);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
+
     app.getSessionManager().logout();
   }
 }
