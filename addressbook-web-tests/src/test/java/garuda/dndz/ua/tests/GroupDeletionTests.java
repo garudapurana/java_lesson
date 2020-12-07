5452e493efe997336b55615cb25bc0ef7f4d6930
garudapurana <73479834+garudapurana@users.noreply.github.com>
2020-12-07 15:08:44 +0200

    51 Fluentинтерфейсы
    51 Fluentинтерфейсы

--- addressbook-web-tests/src/test/java/garuda/dndz/ua/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/garuda/dndz/ua/tests/GroupDeletionTests.java
@@ -12,7 +12,7 @@ public class GroupDeletionTests extends TestBase {
   public void ansurePreconditions(){
     app.goTO().groupPage();
     if(app.group().list().size() == 0){
-      app.group().create(new GroupData("test1", null, null));
+      app.group().create(new GroupData().withName("test1"));
     }
   }
   @Test
