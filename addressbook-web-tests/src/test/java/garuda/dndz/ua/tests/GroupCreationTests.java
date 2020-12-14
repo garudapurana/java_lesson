32453c409c66b88dd22c17e513ee3635a707b1c6
garudapurana <73479834+garudapurana@users.noreply.github.com>
2020-12-14 13:41:03 +0200

    66 Автоматическое закрытие файлов после использования
    66 Автоматическое закрытие файлов после использования

--- addressbook-web-tests/src/test/java/garuda/dndz/ua/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/garuda/dndz/ua/tests/GroupCreationTests.java
@@ -22,46 +22,49 @@ public class GroupCreationTests extends TestBase {
 
   @DataProvider
   public Iterator<Object[]> validGroupsFromXml() throws IOException {
-    BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/groups.xml"));
-    String xml = "";
+    try(BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/groups.xml"))){
+      String xml = "";
 
-    String line = reader.readLine();
-    while(line != null){
-      xml += line;
-      line = reader.readLine();
+      String line = reader.readLine();
+      while(line != null){
+        xml += line;
+        line = reader.readLine();
+      }
+      XStream xStream = new XStream();
+      xStream.processAnnotations(GroupData.class);
+      List<GroupData> groups = (List<GroupData>) xStream.fromXML(xml);
+      return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
     }
-    XStream xStream = new XStream();
-    xStream.processAnnotations(GroupData.class);
-    List<GroupData> groups = (List<GroupData>) xStream.fromXML(xml);
-    return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+
   }
   @DataProvider
   public Iterator<Object[]> validGroupsFromJson() throws IOException {
-    BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/groups.json"));
-    String json = "";
+    try(BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/groups.json"))){
+      String json = "";
 
-    String line = reader.readLine();
-    while (line != null) {
-      json += line;
-      line = reader.readLine();
+      String line = reader.readLine();
+      while (line != null) {
+        json += line;
+        line = reader.readLine();
+      }
+      Gson gson = new Gson();
+      List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>() {
+      }.getType());
+      return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
     }
-    Gson gson = new Gson();
-    List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>() {
-    }.getType());
-    return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
   }
 
   @Test(dataProvider = "validGroupsFromJson")
   public void addGroupTest(GroupData group) {
-    app.goTO().groupPage();
-    Groups before = app.group().all();
-    app.group().create(group);
-    assertThat(app.group().count(),equalTo(before.size()+1));
-    Groups after = app.group().all();
-    assertThat(after, equalTo(
-            before.withAdded(group.withId(after.stream().mapToInt((g)->g.getId()).max().getAsInt()))));
+      app.goTO().groupPage();
+      Groups before = app.group().all();
+      app.group().create(group);
+      assertThat(app.group().count(),equalTo(before.size()+1));
+      Groups after = app.group().all();
+      assertThat(after, equalTo(
+              before.withAdded(group.withId(after.stream().mapToInt((g)->g.getId()).max().getAsInt()))));
+    }
 
-  }
   @Test
   public void addGroupBadTest() {
     app.goTO().groupPage();
